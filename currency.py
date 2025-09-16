3f4e06238c7ef03a6451a09f63bdf798769d8512
SaniaDebbarma <saniadebbarma792@gmail.com>
2025-09-16 23:31:26 +0530

    your commit message

---
+++ currency.py
@@ -0,0 +1,6 @@
+# Write code below 💖
+a = int(input("What do you have left in pesos?"))
+b = int(input("What do you have left in soles?"))
+c = int(input("What do you have left in reais?"))
+result = float(a+b+c)
+print(result)
